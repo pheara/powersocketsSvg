54f10be4536e47c46d26e4c37190ed5d4922f699
moru <e1028092@student.tuwien.ac.at>
2016-10-22 19:55:28 +0200

    Removed commented-out code.

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,14 @@
 'use strict';
 
 var gulp = require('gulp');
-//var sass = require('gulp-sass');
-//var minifyCss = require('gulp-minify-css');
-//var rename = require('gulp-rename');
-//var autoprefixer = require('gulp-autoprefixer');
-//var svgSprite = require('gulp-svg-sprite');
-//var sassImportOnce = require('node-sass-import-once');
 var gulp_jspm = require('gulp-jspm');
 var sourcemaps = require('gulp-sourcemaps');
 var ts = require("gulp-typescript");
-
-
-
 
 gulp.task('default', ['build']);
 gulp.task('build', ['bundlejs']);
 gulp.task('watch', ['bundlejs'], function() {
     gulp.watch('./app/**/*.js', ['bundlejs']);
-    //gulp.watch('./style/**/*.scss', ['sass']);
-    //gulp.watch('./style/**/_*.scss', ['sass']);
-    //gulp.watch('./images/won-icons/**/*.svg', ['iconsprite']);
 });
 
 gulp.task('bundlejs', function(){
@@ -34,30 +22,3 @@ gulp.task('bundlejs', function(){
         .pipe(gulp.dest('./generated/'));
 });
 
-/*
-gulp.task('bundlejs', function(){
-    return gulp.src('app/test.ts')
-        .pipe(sourcemaps.init())
-        .pipe(gulp_jspm({inject: true}))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('./generated/'));
-});
-*/
-
-/*
-gulp.task('bundlejs', function(){
-    return gulp.src('app/app_jspm.js')
-        .pipe(sourcemaps.init())
-        .pipe(gulp_jspm({inject: true}))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('./generated/'));
-});
-
-
-var tsProject = ts.createProject("tsconfig.json");
-gulp.task("typescript", function () {
-    return tsProject.src()
-        .pipe(tsProject())
-        .js.pipe(gulp.dest("./generated/"));
-});
-*/
