fb4f860057a6cca9efa9327f675f5d7dd390bae7
moru <e1028092@student.tuwien.ac.at>
2016-10-22 20:27:40 +0200

    Some comments/links/references.

--- app/test.ts
+++ app/test.ts
@@ -6,3 +6,43 @@ window.SVG4dbg = SVG;
 
 console.log('hello typescript', foo);
 
+
+/*
+
+declare var Raphael: any; //imported in index.html
+
+
+//TODO take a look at the lightweight svg.js
+
+
+
+importing svg data
+
+* https://github.com/wout/svg.js#import--export-svg
+* https://github.com/wout/raphael-svg-import (deprecated for svg.js)
+
+
+parsing path data
+
+* https://github.com/hughsk/svg-path-parser
+* https://www.npmjs.com/package/parse-svg
+* https://github.com/canvg/canvg
+
+
+
+
+
+console.log(Raphael);
+
+// Creates canvas 320 × 200 at 10, 50
+// var paper = Raphael(10, 50, 320, 200);
+//
+// // Creates circle at x = 50, y = 40, with radius 10
+// var circle = paper.circle(50, 40, 10);
+// // Sets the fill attribute of the circle to red (#f00)
+// circle.attr("fill", "#f00");
+//
+// // Sets the stroke attribute of the circle to white
+// circle.attr("stroke", "#fff");
+//
+ */
