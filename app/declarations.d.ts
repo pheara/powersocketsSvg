6f9eeb4f30d4e191379659a763922fd251313fc1
Moru <kevin.singer@student.tuwien.ac.at>
2016-11-06 22:11:50 +0100

    More code-styling.

--- app/declarations.d.ts
+++ app/declarations.d.ts
@@ -6,25 +6,25 @@
 declare module "svg-path-parser";
 
 interface Switch {
-  element: SVGPathElement,
+  element: SVGPathElement;
 }
 
 interface Rectangle {
-  pos: Point, // left-upper corner
-  width: number,
-  height: number,
-  element: SVGRectElement
+  pos: Point; // left-upper corner
+  width: number;
+  height: number;
+  element: SVGRectElement;
 }
 
 interface Generator extends Rectangle {}
 interface Socket extends Rectangle {}
 
 interface Powerline {
-  start: Point
-  end: Point
-  element: SVGPathElement
+  start: Point;
+  end: Point;
+  element: SVGPathElement;
 }
 interface Point {
-  x: number,
-  y: number,
+  x: number;
+  y: number;
 }
