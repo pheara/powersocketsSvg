e6310930d1969e2ec052c2677e210c52339926af
moru <e1028092@student.tuwien.ac.at>
2016-11-06 14:50:42 +0100

    Added util function to check membership in any collection.

--- app/utils.ts
+++ app/utils.ts
@@ -5,3 +5,15 @@ export function valueOr<T>(value: T | undefined | null, deflt: T): T {
 export function hasJSType(obj: any) {
   return Object.prototype.toString.call(obj).slice(8, -1);
 }
+
+export function contains<T>( xs: T[] | Iterable<T>, x: T): boolean {
+  if((<T[]>xs).indexOf) {
+    // assumes that internal implementation is more optimized
+    return (<T[]>xs).indexOf(x) >= 0;
+  } else {
+    for(const el of xs) {
+      if(el === x) return true;
+    }
+    return false;
+  }
+}
