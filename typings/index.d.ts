9778698f47eccdf6182fac0a244bb67f49e5346e
Moru <kevin.singer@student.tuwien.ac.at>
2016-11-07 02:10:47 +0100

    Added immutablejs to dependencies.

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,3 +1,5 @@
+/// <reference path="globals/immutable/index.d.ts" />
 /// <reference path="globals/svgjs/index.d.ts" />
 /// <reference path="globals/whatwg-fetch/index.d.ts" />
 /// <reference path="globals/whatwg-streams/index.d.ts" />
+/// <reference path="modules/immutable/index.d.ts" />
