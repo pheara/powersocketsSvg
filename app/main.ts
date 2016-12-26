f3f5a394e587c928c4c300da9c92da6336934b7c
Moru <kevin.singer@student.tuwien.ac.at>
2016-12-26 21:17:19 +0100

    Starting at level0 again.

--- app/main.ts
+++ app/main.ts
@@ -100,7 +100,7 @@ let currentMapData: MapData;
 let unregisterDebugMarker: Array<() => void> = [];
 let touchedSockets: Set<Socket> = new Set<Socket>();
 let levelTimerId: number | undefined;
-let currentLevelNr: number = 2; // increase to start at higher level
+let currentLevelNr: number = 0; // increase to start at higher level
 
 let iconPrototypes: {
   shocked: SVGSVGElement,
