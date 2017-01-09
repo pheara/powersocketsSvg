6d7fd5eaf697e3651d93633fed8e898b8b6e6640
moru <e1028092@student.tuwien.ac.at>
2017-01-09 19:52:57 +0100

    Added scoring-config for new levels.

--- app/config.ts
+++ app/config.ts
@@ -17,48 +17,95 @@ export const levels: Array<{
     missedOpportunityPenalty: number, // per second
     takenOpportunityPoints: number // per second
 }> = deepFreeze([
+
   { // level0
     timeLimit: 30,
     initialPoints: 30,
     shockPenalty: 27,
     missedOpportunityPenalty: 10,
-    takenOpportunityPoints: 20,
+    takenOpportunityPoints: 40,
   },
   { // level1
     timeLimit: 30,
     initialPoints: 30,
     shockPenalty: 28,
-    missedOpportunityPenalty: 6,
-    takenOpportunityPoints: 12,
+    missedOpportunityPenalty: 10,
+    takenOpportunityPoints: 50,
   },
   { // level2
     timeLimit: 30,
     initialPoints: 30,
     shockPenalty: 10,
     missedOpportunityPenalty: 2,
-    takenOpportunityPoints: 17,
+    takenOpportunityPoints: 50,
   },
   { // level3
     timeLimit: 30,
     initialPoints: 30,
     shockPenalty: 30,
     missedOpportunityPenalty: 8,
-    takenOpportunityPoints: 12,
+    takenOpportunityPoints: 50,
   },
   { // level4
     timeLimit: 30,
     initialPoints: 30,
     shockPenalty: 31,
     missedOpportunityPenalty: 8,
-    takenOpportunityPoints: 12,
+    takenOpportunityPoints: 50,
   },
   { // level5
     timeLimit: 30,
     initialPoints: 30,
     shockPenalty: 31,
     missedOpportunityPenalty: 5,
+    takenOpportunityPoints: 50,
+  },
+
+    
+  { // level6
+    timeLimit: 30,
+    initialPoints: 30,
+    shockPenalty: 27,
+    missedOpportunityPenalty: 20,
+    takenOpportunityPoints: 40,
+  },
+  { // level7
+    timeLimit: 30,
+    initialPoints: 30,
+    shockPenalty: 28,
+    missedOpportunityPenalty: 6,
+    takenOpportunityPoints: 12,
+  },
+  { // level8
+    timeLimit: 30,
+    initialPoints: 30,
+    shockPenalty: 10,
+    missedOpportunityPenalty: 2,
+    takenOpportunityPoints: 17,
+  },
+  { // level9
+    timeLimit: 30,
+    initialPoints: 30,
+    shockPenalty: 30,
+    missedOpportunityPenalty: 8,
+    takenOpportunityPoints: 12,
+  },
+  { // level10
+    timeLimit: 30,
+    initialPoints: 30,
+    shockPenalty: 31,
+    missedOpportunityPenalty: 8,
+    takenOpportunityPoints: 12,
+  },
+  { // level11
+    timeLimit: 30,
+    initialPoints: 30,
+    shockPenalty: 31,
+    missedOpportunityPenalty: 5,
     takenOpportunityPoints: 8,
   },
+
+
   { // level6 - end screen
     timeLimit: 999999,
     initialPoints: 0,
